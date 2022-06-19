2639fdaa29f5e279349c5696dfe7072072b67e9a
prafultandekar <prafultandekar1997@gmail.com>
2022-06-19 23:47:20 +0530

    NOW-DONE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // import logo from './logo.svg';
-<<<<<<< HEAD
 import { useContext, useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
@@ -92,34 +91,11 @@ function App() {
                
         </div>
        )}
-=======
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage/LoginPage';
-import SignPage from './pages/Signpage/SignPage';
-// import header from './components/Header/header';
-// import Header from "./components/Header/header"
-
-function App() {
-  return (
-    <div className="App">
-
-
-      <Routes>
-        <Route path="LoginPage" element={<LoginPage/>} />
-        <Route path="/" element={<SignPage/>}/>
-      </Routes>
-     
-     
->>>>>>> 0bc88167daece1ef3a5cf5bf90e49f0051dcb66d
     </div>
   );
 }
 
 export default App;
-<<<<<<< HEAD
 
         
 
-=======
->>>>>>> 0bc88167daece1ef3a5cf5bf90e49f0051dcb66d
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -22,26 +21,6 @@ root.render(
         </BrowserRouter>
       </ChakraProvider>
     </Provider>
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
-import { BrowserRouter } from "react-router-dom"
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-import { ChakraProvider } from "@chakra-ui/react"
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-    <BrowserRouter>
-      <App />
-      </BrowserRouter>
-    </ChakraProvider>
-    
->>>>>>> 0bc88167daece1ef3a5cf5bf90e49f0051dcb66d
   </React.StrictMode>
 );
 
